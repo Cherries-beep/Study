58744bebc5b0443d60f4d3022245dd2ad681d0c2
Vikaa <viktoriya_vlasova_00@list.ru>
2023-08-24 19:33:47 +0700

    List, entering trought loop

--- length=5.py
+++ length=5.py
@@ -1,7 +1,7 @@
 length=5
 mas = [0] * length 
 for i in range(length):
-    mas[i] =int(input())
+    mas[i] = int(input())
 
 print('Наименьшее число =',min(mas))
 print('Наибольшее число =',max(mas))
